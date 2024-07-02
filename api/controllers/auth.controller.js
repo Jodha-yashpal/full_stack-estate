f62ca28e7622a3e75524172967d37e97084f9ae3
Yashpal <yashpalsinghjodha2527@gmail.com>
2024-07-02 17:33:28 +0530

    login and logout endpoint added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,27 +1,31 @@
-import {ApiError} from '../lib/ApiError.js'
-import {ApiResponse} from '../lib/ApiResponse.js'
+import { ApiError } from '../lib/ApiError.js'
+import { ApiResponse } from '../lib/ApiResponse.js'
 import bcrypt from 'bcrypt'
 import prisma from '../lib/prisma.js'
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const registerUser = async (req, res) => {
     try {
         //fetch
-        const {username, email, password} = req.body;
+        const { username, email, password } = req.body;
 
         //validation
-        if(!username || !email || !password){
+        if (!username || !email || !password) {
             throw new ApiError(402, "all fields are required")
         }
 
         //check if user exists
         const existingUser = await prisma.user.findUnique({
-            where:{
+            where: {
                 username: username
             }
         })
 
         if (existingUser) {
-            throw new ApiError(410, "user already exists"); 
+            throw new ApiError(410, "user already exists");
         }
 
         //hash the password
@@ -29,7 +33,7 @@ const registerUser = async (req, res) => {
 
         //create a new user
         const newUser = await prisma.user.create({
-            data:{
+            data: {
                 username,
                 email,
                 password: hashedPassword
@@ -39,22 +43,93 @@ const registerUser = async (req, res) => {
         //return result
         return res.status(201).json(
             new ApiResponse(201, newUser, "success")
-        ) 
+        )
 
-        
+
     } catch (error) {
         console.log(error);
 
         if (error instanceof ApiError) {
             return res
-            .status(error.statusCode)
-            .json(
-                new ApiResponse(error.statusCode, null, error.message)
-            )
+                .status(error.statusCode)
+                .json(
+                    new ApiResponse(error.statusCode, null, error.message)
+                )
         }
 
         throw new ApiError(500, "Failed to create user?");
     }
 }
 
-export {registerUser}
+const login = async (req, res) => {
+    try {
+        //fetch 
+        const { username, password } = req.body;
+
+        //validation
+        if (!username || !password) {
+            throw new ApiError(402, "both fields are required")
+        }
+
+        //check if user exists
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+
+        if (!user) {
+            throw new ApiError(401, "Invalid Credentials!")
+        }
+
+        //check if password is valid
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            throw new ApiError(401, "Invalid Credentials!")
+        }
+
+        //generate cookie token and send to the user
+
+        const token = jwt.sign({
+            id: user.id
+        },
+            process.env.ACCESS_TOKEN_KEY,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+
+        const age = 1000*60*60*24*7
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            // secure: true
+            maxAge: age
+        })
+            .status(200)
+            .json(
+                new ApiResponse(200, null, "login Successfull")
+            )
+
+    } catch (error) {
+        console.log("error", error)
+
+        if (error instanceof ApiError) {
+            return res
+                .status(error.statusCode)
+                .json(
+                    new ApiResponse(error.statusCode, null, error.message)
+                )
+        }
+        throw new ApiError(500, "Failed to login the user")
+    }
+}
+
+const logout = (req, res) => {
+    res.clearCookie("token")
+        .status(200)
+        .json(
+            new ApiResponse(200, null, "logout Successfull")
+        )
+}
+
+export { registerUser, login, logout }
