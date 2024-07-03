0dfd5d4da123c77efc1c614f73d7c43f4fcc6047
Yashpal <yashpalsinghjodha2527@gmail.com>
2024-07-03 16:43:49 +0530

    logout added in profile page

--- client/src/routes/profilePage/ProfilePage.jsx
+++ client/src/routes/profilePage/ProfilePage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
-import './profilePage.scss'
-import List from '../../components/list/List'
-import Chat from '../../components/chat/Chat'
+import React from "react";
+import "./profilePage.scss";
+import List from "../../components/list/List";
+import Chat from "../../components/chat/Chat";
+ import apiRequest from "../../lib/apiRequest.js"
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
+
+  const navigate = useNavigate()
+
+  const handleLogout = async (e) => {
+    try {
+      const res = apiRequest.post("/auth/logout")
+      localStorage.removeItem("user")
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <div className='profilePage'>
+    <div className="profilePage">
       <div className="details">
         <div className="wrapper">
           <div className="title">
@@ -13,11 +28,20 @@ function ProfilePage() {
             <button>Update Profile</button>
           </div>
           <div className="info">
-            <span>Avatar:
-              <img src="https://images.pexels.com/photos/24375005/pexels-photo-24375005/free-photo-of-young-brunette-posing-among-tropical-green-leaves.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+            <span>
+              Avatar:
+              <img
+                src="https://images.pexels.com/photos/24375005/pexels-photo-24375005/free-photo-of-young-brunette-posing-among-tropical-green-leaves.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+              />
             </span>
-            <span>Username: <b>Jane Doe</b></span>
-            <span>email: <b>jane@gmail.com</b></span>
+            <span>
+              Username: <b>Jane Doe</b>
+            </span>
+            <span>
+              email: <b>jane@gmail.com</b>
+            </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
@@ -28,7 +52,6 @@ function ProfilePage() {
             <h1>Saved List</h1>
           </div>
           <List />
-          
         </div>
       </div>
 
@@ -38,7 +61,7 @@ function ProfilePage() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
